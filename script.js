164263348280ede21be6f6d2b9125d8e8bc64a19
Joe Wickes <joe.c.wickes@gmail.com>
2020-10-19 14:04:58 -0400

    Remove unnecessary comments

--- script.js
+++ script.js
@@ -1,67 +1,27 @@
-/*
-$(function() {
-  $('form').on('submit', (event) => {
-    event.preventDefault();
-
-    function fizzBuzz(num) {
-      console.log('started');
-      num = parseInt(num);
-      let results = [];
-      for (let i = 0; i <= num; i++) {
-        console.log(i);
-        if (i === 0) {
-          results.push($(`<div class="fizz-buzz-item">
-          <span>${i}</span>
-          </div>`));
-        } else if (i % 3 === 0 && i % 5 === 0) {
-          results.push($(`<div class="fizz-buzz-item">
-          <span>FizzBuzz</span>
-          </div>`));
-        } else if (i % 3 === 0) {
-          results.push($(`<div class="fizz-buzz-item">
-          <span>Fizz</span>
-          </div>`));
-        } else if (i % 5 === 0) {
-          results.push($(`<div class="fizz-buzz-item">
-          <span>Buzz</span>
-          </div>`));
-        } else {
-          results.push($(`<div class="fizz-buzz-item"><span>${i}</span></div>`));
-        }
-      }
-      console.log(results);
-      console.log('finishing');
-      $('js-results').append(results);
+function fizzBuzz(num) {
+  let appArr = [];
+    
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>FizzBuzz</span></div>');
+    } else if (i % 3 === 0) {
+      appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>Fizz</span></div>');
+    } else if (i % 5 === 0) {
+      appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>Buzz</span></div>');
+    } else {
+      appArr.push(`<div class="fizz-buzz-item fizzbuzz"><span>${i}</span></div>`);
     }
+  }
 
-    let numberInput = $('#number').val();
-    console.log(numberInput);
-    fizzBuzz(numberInput);
-    $('#number').val('');
-  });
-});
-*/
+  $('.js-results').append(appArr);
+}
 
 $(function() {
   $('form').on('submit', function(event) {
-    event.preventDefault(); // Works
+    event.preventDefault();
 
-    let numInput = parseInt($('#number').val()); // Works
-
-    let appArr = [];
-
-    for (let i = 1; i <= numInput; i++) { // Works in console
-      if (i % 3 === 0 && i % 5 === 0) {
-        appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>FizzBuzz</span></div>');
-      } else if (i % 3 === 0) {
-        appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>Fizz</span></div>');
-      } else if (i % 5 === 0) {
-        appArr.push('<div class="fizz-buzz-item fizzbuzz"><span>Buzz</span></div>');
-      } else {
-        appArr.push(`<div class="fizz-buzz-item fizzbuzz"><span>${i}</span></div>`);
-      }
-    }
-
-    $('.js-results').append(appArr);
+    let numInput = parseInt($('#number').val());
+    
+    fizzBuzz(numInput);
   });
 });
